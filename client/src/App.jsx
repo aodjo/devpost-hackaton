95c9d453675641349c696b9b10e70e7f52ec62ae
Junsung Lee <jjoons2010@gmail.com>
2026-02-22 05:56:47 +0900

    dev: add openstreetmap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
+import { CircleMarker, MapContainer, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 import './App.css'
 
 function App() {
+  const seoulCityHall = [37.5665, 126.978]
+
   return (
-    <>
-      <h1 className='text-red-500'>Hello, world!</h1>
-    </>
+    <main className="app">
+      <h1>Hello, world!</h1>
+      <MapContainer center={seoulCityHall} zoom={13} scrollWheelZoom className="map">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <CircleMarker center={seoulCityHall} radius={12}>
+          <Popup>Seoul City Hall</Popup>
+        </CircleMarker>
+      </MapContainer>
+    </main>
   )
 }
 
